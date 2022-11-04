613efdf333d1c37a7f929e27600d41940da7f59c
MaskerDog <37284851+MaskerDog@users.noreply.github>
2022-11-04 09:02:06 +0900

    Initial Commit

---
+++ UserDefaultsSample/UserDefaultsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsSampleApp.swift
+//  UserDefaultsSample
+//
+//  Created by npc on 2022/11/04.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
